ed74029a58145d32596de1b9a16466a69100cc12
TPSSOFT\man.pham <tuephamman@gmail.com>
2020-10-12 18:01:38 +0700

    push code

--- QuanLiNhaTro/frm_ThemThietBi.cs
+++ QuanLiNhaTro/frm_ThemThietBi.cs
@@ -25,7 +25,6 @@ namespace QuanLiNhaTro
         void load_grid()
         {
             var db = kn.connosql;
-            var collection = db.GetCollection<TB>("TBIiiiiiii");
             var query = collection.AsQueryable<TB>().ToList();
             dtgv_thietbi.Rows.Clear();
             foreach (var query1 in query)
